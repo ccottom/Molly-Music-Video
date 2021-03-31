2af8c947b7fda04de7f6250e0d587d4f93acf61c
Chloe <ccotto@artic.edu>
2021-03-31 11:28:20 -0500

    Add files via upload

--- chloe.js
+++ chloe.js
@@ -1,36 +1,38 @@
-// create a new hydra-synth instance
-const canvas=document.getElementById("myCanvas")
-canvas.width=window.innerWidth
-canvas.height=window.innerHeight
-new window.Hydra({
-  canvas: canvas,
-  detectAudio: false
-})
-const button=document.getElementById("myButton")
+document.addEventListener("DOMContentLoaded", function () {
+  // create a new hydra-synth instance
+  const canvas = document.getElementById("myCanvas");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  new window.Hydra({
+    canvas: canvas,
+    detectAudio: false,
+  });
+  const button = document.getElementById("myButton");
 
-function hydracode () {
-  canvas.style.display= 'block'
-  button.style.display= 'none'
-  unmute ()
+  function hydracode() {
+    canvas.style.display = "block";
+    button.style.display = "none";
+    unmute();
 
-  osc().out()
-  s0.initVideo("MollyMP4.mp4")
-  src(s0)
-  .rotate(0,0)
-  .repeat()
-  .invert(0,0)
-  //.saturateHue()
-  .scale(1)
-  //.mult(osc(0,-0.1,0.0))
-  .color(1,0.6,-1)
-  .out()
-}
+    osc().out();
+    s0.initVideo("MollyMP4.mp4");
+    src(s0)
+      .rotate(0, 0)
+      .repeat()
+      .invert(0, 0)
+      //.saturateHue()
+      .scale(1)
+      //.mult(osc(0,-0.1,0.0))
+      .color(1, 0.6, -1)
+      .out();
+  }
 
-button.addEventListener('click', hydracode)
+  button.addEventListener("click", hydracode);
+});
 
-function unmute () {
+function unmute() {
   // if video hasn't loaded, try again in 10 miliseconds
-  if (!s0.src) return setTimeout(unmute, 10)
+  if (!s0.src) return setTimeout(unmute, 10);
   // otherwise unmute the video
-  s0.src.muted = false
+  s0.src.muted = false;
 }
